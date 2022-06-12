ca9b6a4d889afd0638b859c03963cd5ed5b129d4
Vadim <vadim_kimm@icloud.com>
2022-06-12 18:48:31 +0300

    Initial Commit

---
+++ HW14-Photos-Albums-Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14-Photos-Albums-Screen
+//
+//  Created by Vadim Kim on 12.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
